109c3e1e91510a703b0a5d184db5541e66cf78d2
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2024-12-23 13:03:27 +0100

    Initial MathPlayground app setup

---
+++ src/main/kotlin/rs/playgroundmath/playgroundmath/PlaygroundMathApplication.kt
@@ -0,0 +1,11 @@
+package rs.playgroundmath.playgroundmath
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlaygroundMathApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlaygroundMathApplication>(*args)
+}
---
+++ src/test/kotlin/rs/playgroundmath/playgroundmath/PlaygroundMathApplicationTests.kt
@@ -0,0 +1,13 @@
+package rs.playgroundmath.playgroundmath
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlaygroundMathApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
