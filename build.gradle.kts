967504cfebccac89952fc87feacde5c30f0acaae
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2024-12-24 15:54:59 +0100

    Added mysql and JPA dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
