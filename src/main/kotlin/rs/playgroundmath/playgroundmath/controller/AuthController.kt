059099fe8e749de1bc9e6cd91efeff8898bda063
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2024-12-26 13:56:40 +0100

    Added implemented basic login and test not protected route

--- src/main/kotlin/rs/playgroundmath/playgroundmath/controller/AuthController.kt
+++ src/main/kotlin/rs/playgroundmath/playgroundmath/controller/AuthController.kt
@@ -1,19 +1,28 @@
 package rs.playgroundmath.playgroundmath.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import rs.playgroundmath.playgroundmath.command.UserLoginCommand
 import rs.playgroundmath.playgroundmath.command.UserRegisterCommand
+import rs.playgroundmath.playgroundmath.config.JwtUtil
 import rs.playgroundmath.playgroundmath.dto.UserRegisterDto
+import rs.playgroundmath.playgroundmath.payload.request.LoginRequest
 import rs.playgroundmath.playgroundmath.payload.request.RegisterRequest
 import rs.playgroundmath.playgroundmath.payload.response.Response
 
 @RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
-    private val userRegisterCommand: UserRegisterCommand
+    private val userRegisterCommand: UserRegisterCommand,
+    private val userLoginCommand: UserLoginCommand,
+    private val authenticationManager: AuthenticationManager,
+    private val jwtUtil: JwtUtil
 ) {
 
     @PostMapping("/register")
@@ -38,5 +47,21 @@ class AuthController(
         )
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<Any> {
+        val authentication = authenticationManager.authenticate(
+            UsernamePasswordAuthenticationToken(loginRequest.email, loginRequest.password)
+        )
+
+        val token = jwtUtil.generateToken(authentication.name)
+
+        return ResponseEntity.ok(token)
+    }
+
+    @GetMapping("/test")
+    fun test(): ResponseEntity<Any> {
+        return ResponseEntity.ok("test")
     }
 }
