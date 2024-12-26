e5ad40d2521f174f8e24dde06576bbe82cd89e97
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2024-12-26 13:56:01 +0100

    Added jwt dependencies to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,9 @@ repositories {
 }
 
 dependencies {
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
