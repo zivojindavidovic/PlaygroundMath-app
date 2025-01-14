6f77368a5bd5bec703c5a2ead8f5e743f3827273
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-01-14 19:19:52 +0100

    Added maximum accounts reached per user handling

--- src/main/kotlin/rs/playgroundmath/playgroundmath/advice/GlobalExceptionHandler.kt
+++ src/main/kotlin/rs/playgroundmath/playgroundmath/advice/GlobalExceptionHandler.kt
@@ -1,9 +1,10 @@
-package rs.playgroundmath.playgroundmath.exception
+package rs.playgroundmath.playgroundmath.advice
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import rs.playgroundmath.playgroundmath.exceptions.AccountMaximumPerUserException
 import rs.playgroundmath.playgroundmath.exceptions.UserAlreadyExistsException
 
 @RestControllerAdvice
@@ -14,6 +15,16 @@ class GlobalExceptionHandler {
         val response: Map<String, String> = mapOf(
             "error" to "User Already Exists",
             "message" to (ex.message ?: "User with this email already exists")
+        )
+
+        return ResponseEntity(response, HttpStatus.CONFLICT)
+    }
+
+    @ExceptionHandler(AccountMaximumPerUserException::class)
+    fun handleAccountMaximumPerUserException(ex: AccountMaximumPerUserException): ResponseEntity<Map<String, String>> {
+        val response: Map<String, String> = mapOf(
+            "error" to "Account Maximum Per User",
+            "message" to (ex.message ?: "Account maximum per user reached")
         )
 
         return ResponseEntity(response, HttpStatus.CONFLICT)
---
+++ src/main/kotlin/rs/playgroundmath/playgroundmath/exceptions/AccountMaximumPerUserException.kt
@@ -0,0 +1,3 @@
+package rs.playgroundmath.playgroundmath.exceptions
+
+class AccountMaximumPerUserException(message: String): RuntimeException(message)
