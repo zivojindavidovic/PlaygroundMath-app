16316cbdc92adce79d1028a47387a5623cc416fd
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-07 16:26:45 +0100

    Added validation and mail dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
