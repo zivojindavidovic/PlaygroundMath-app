a228f689eee68ea95adf4e0b9ad50784f47f0a65
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-07 16:37:15 +0100

    Added validation for UserRegisterRequest

--- src/main/kotlin/rs/playgroundmath/playgroundmath/payload/request/UserRegisterRequest.kt
+++ src/main/kotlin/rs/playgroundmath/playgroundmath/payload/request/UserRegisterRequest.kt
@@ -1,9 +1,21 @@
 package rs.playgroundmath.playgroundmath.payload.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 import rs.playgroundmath.playgroundmath.enums.RoleType
 
 data class UserRegisterRequest(
+    @field:NotBlank(message = "E-adresa mora biti uneta")
+    @field:Email(message = "E-adresa mora biti u ispravnom formatu")
     val email: String,
+    @field:NotBlank(message = "Lozinka mora biti uneta")
+    @field:Size(min = 5, message = "Lozinka mora imati najmanje 5 karaktera")
+    @field:Pattern(
+        regexp = "^(?=.*\\d)(?=.*[!@#\$%^&*._+=-]).+$",
+        message = "Lozinka mora sadrzati barem jedan broj i barem jedan specijalni karakter"
+    )
     val password: String,
     val accountType: RoleType = RoleType.PARENT
 )
