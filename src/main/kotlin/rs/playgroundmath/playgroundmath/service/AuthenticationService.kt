d5f8111a3248ada9a12ddeb477cbabc03239a01b
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2025-02-07 17:42:33 +0100

    Added auth interface and additional login for error handling on authenticate action

--- src/main/kotlin/rs/playgroundmath/playgroundmath/service/AuthenticationService.kt
+++ src/main/kotlin/rs/playgroundmath/playgroundmath/service/AuthenticationService.kt
@@ -1,45 +1,9 @@
 package rs.playgroundmath.playgroundmath.service
 
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.stereotype.Service
-import rs.playgroundmath.playgroundmath.config.JwtProperties
-import rs.playgroundmath.playgroundmath.enums.RoleType
 import rs.playgroundmath.playgroundmath.payload.request.AuthenticationRequest
 import rs.playgroundmath.playgroundmath.payload.response.AuthenticationResponse
-import java.util.*
 
-@Service
-class AuthenticationService(
-    private val authenticationManager: AuthenticationManager,
-    private val userDetailsService: CustomUserDetailsService,
-    private val tokenService: TokenService,
-    private val jwtProperties: JwtProperties,
-    private val userService: UserService
-) {
+interface AuthenticationService {
 
-    fun authentication(authRequest: AuthenticationRequest): AuthenticationResponse {
-        authenticationManager.authenticate(
-            UsernamePasswordAuthenticationToken(
-                authRequest.email,
-                authRequest.password
-            )
-        )
-
-        val user = userDetailsService.loadUserByUsername(authRequest.email)
-        val accessToken = tokenService.generateToken(
-            userDetails = user,
-            expirationDate = Date(System.currentTimeMillis() + jwtProperties.accessTokenExpiration)
-        )
-
-        val loggedInUser = userService.getUserByEmail(user.username)
-        val isTeacher = loggedInUser!!.role.roleType == RoleType.TEACHER
-
-        return AuthenticationResponse(
-            accessToken = accessToken,
-            userId = loggedInUser!!.userId,
-            email = loggedInUser.email,
-            isTeacher = isTeacher
-        )
-    }
+    fun authenticate(authRequest: AuthenticationRequest): AuthenticationResponse
 }
---
+++ src/main/kotlin/rs/playgroundmath/playgroundmath/service/AuthenticationServiceImpl.kt
@@ -0,0 +1,54 @@
+package rs.playgroundmath.playgroundmath.service
+
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.stereotype.Service
+import rs.playgroundmath.playgroundmath.config.JwtProperties
+import rs.playgroundmath.playgroundmath.enums.RoleType
+import rs.playgroundmath.playgroundmath.enums.Status
+import rs.playgroundmath.playgroundmath.exceptions.UserStatusNotActiveException
+import rs.playgroundmath.playgroundmath.payload.request.AuthenticationRequest
+import rs.playgroundmath.playgroundmath.payload.response.AuthenticationResponse
+import java.util.*
+
+@Service
+class AuthenticationServiceImpl(
+    private val authenticationManager: AuthenticationManager,
+    private val userDetailsService: CustomUserDetailsService,
+    private val tokenService: TokenService,
+    private val jwtProperties: JwtProperties,
+    private val userService: UserService
+): AuthenticationService {
+
+    override fun authenticate(authRequest: AuthenticationRequest): AuthenticationResponse {
+        authenticationManager.authenticate(
+            UsernamePasswordAuthenticationToken(
+                authRequest.email,
+                authRequest.password
+            )
+        )
+
+        val user = userDetailsService.loadUserByUsername(authRequest.email)
+        val email = user.username
+        val applicationUser = userService.getUserByEmail(email)
+
+        if (applicationUser!!.status == Status.PENDING) {
+            throw UserStatusNotActiveException("Nalog sa ovom e adresom nije aktivan")
+        }
+
+        val accessToken = tokenService.generateToken(
+            userDetails = user,
+            expirationDate = Date(System.currentTimeMillis() + jwtProperties.accessTokenExpiration)
+        )
+
+        val loggedInUser = userService.getUserByEmail(user.username)
+        val isTeacher = loggedInUser!!.role.roleType == RoleType.TEACHER
+
+        return AuthenticationResponse(
+            accessToken = accessToken,
+            userId = loggedInUser.userId,
+            email = loggedInUser.email,
+            isTeacher = isTeacher
+        )
+    }
+}
